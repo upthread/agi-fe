60da340eb76146308b54474cd2ed83b22320a2f8
adeoye adela <adeoye.adela@gmail.com>
2019-11-10 12:23:54 +0100

    CTA section

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,45 +1,127 @@
-import React from 'react'
+import React, { useState } from "react";
+
 import {
-    Body,
-    Splash,
-    Banner,
-    Feature,
-    Portfolio,
-    CTA,
-    // SplashImg
-} from './styles'
-import { Container, Row, Col } from 'reactstrap';
+  Body,
+  Splash,
+  Banner,
+  Feature,
+  Portfolio,
+  CTA
+  // SplashImg
+} from "./styles";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from "reactstrap";
+import data from "./data";
+// need a fixed sidebar so contact
 
- // need a fixed sidebar so contact
+const Home = props => {
+  const { buttonLabel, className } = props;
 
-const Home = () => {
-    return (
-        <Body>
-            <Splash>
-                
-                <h1 id='Name'>AGI</h1>
-                <p>A broker You can Trust</p>
-                <footer>
-					<a href="#banner" class="button style2 scrolly-middle">Get Started</a>
-                    </footer>
-                    <div className="line"></div>
-            </Splash>
-            <Banner>
-                <h3>AG international</h3>
-                <p>Web Enabled Real Estate Brokerage</p>
-            </Banner>
-            <Feature>
+  //   const [modal, setModal] = useState(false);
 
-            </Feature>
-            <Portfolio>
+  //   const toggle = (item) => {
+  //     console.log(item.title)
+  //     setModal(!modal);}
+  return (
+    <Body>
+      <Splash>
+        <h1 id="Name">AGI</h1>
+        <p>A broker You can Trust</p>
+        <footer>
+          <a href="#banner" class="button style2 scrolly-middle">
+            Get Started
+          </a>
+        </footer>
+        <div className="line"></div>
+      </Splash>
+      <Banner>
+        <h3>AG international</h3>
+        <p>Web Enabled Real Estate Brokerage</p>
+      </Banner>
+      <Feature></Feature>
+      <Portfolio>
+        <Container>
+          
+            <Row>
+              <Col className="assure" sm="12" xs="12" md={{ size: 6, offset: 3 }}>
+              <h2 class="sub-head">
+                What Makes Us <br />
+                Different?
+              </h2>
+              <p>We offer all AGI agents great value from their
+                brokerage, whilst supplying an amazing amount of training and
+                support at the same time!</p>
+              </Col>
+            </Row>
+          <Row>
+            {data.map((item, index) => (
+              <Col sm="3" xs="12">
+                <Card>
+                  <CardImg
+                    top
+                    width="100%"
+                    src={item.image}
+                    alt="Card image cap"
+                  />
 
-            </Portfolio>
-            <CTA>
+                  <CardBody>
+                    <CardTitle>
+                      {" "}
+                      <h4>{item.title}</h4>
+                    </CardTitle>
+                    <CardText>{item.content}</CardText>
+                    {/* <Button onClick={toggle(item.title)}>    Read More</Button> */}
+                    {/* <Modal
+                      isOpen={modal}
+                      backdrop={index>3? true:false}
+                      toggle={toggle}
+                      className={className}
+                    >
+                        {/* {console.log(item.title)} */}
+                    {/* <ModalHeader toggle={toggle}>{item.title}</ModalHeader>
+                      <ModalBody>
+                       {item.content}
+                      </ModalBody>
+                      <ModalFooter></ModalFooter>
+                    </Modal> */}
+                  </CardBody>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+      <div className="join-sec">
+       <Row>
+       <div class="col-md-9 col-12">
+              <h2>Join AGI today</h2>
+              <h4>
+                If you want to grow your real estate career into a business.
+              </h4>
+            </div>
+            <div class="col-md-3 col-12">
+              <a href="join-agi.html" class="join-btn">Join AGI</a>
+              <div class="clearfix"></div>
+            </div>
+       </Row>
+       </div>
+        </Container>
+      </Portfolio>
+      <CTA></CTA>
+    </Body>
+  );
+};
 
-            </CTA>
-        </Body>
-        
-    )
-}
-
-export default Home
+export default Home;
--- src/Home/styles.js
+++ src/Home/styles.js
@@ -7,10 +7,10 @@ export const Body = styled.section`
   background-position: top left, center 0;
   background-attachment: fixed, fixed;
   font-family: "Source Sans Pro", sans-serif;
-  font-size: 18pt;
-  line-height: 1.75em;
+  // font-size: 18pt;
+  // line-height: 1.75em;
   font-weight: 300;
-  letter-spacing: 1px;
+  // letter-spacing: 1px;
   color: #3a3939;
   text-shadow: 0 0 0.5px rgba(58, 57, 57, 0.25);
   -webkit-text-stroke: 0.25px;
@@ -40,8 +40,8 @@ export const Splash = styled.section`
     font-size: 140px;
     font-family: serif;
     color: white;
-    margin-top:-30px;
-    margin-bottom:30px;
+    margin-top: -30px;
+    margin-bottom: 30px;
     // color: #d1d1ff;
     // color: #6e7277
 
@@ -71,72 +71,70 @@ export const Splash = styled.section`
     letter-spacing: 3px;
     padding: 0;
     margin-top: 0.35em;
-    width:400px;
-    margin:0.35em auto;
-
+    width: 400px;
+    margin: 0.35em auto;
 
     &:before {
       content: "";
       display: block;
-      width:inherit;
+      width: inherit;
       border-top: solid 1px rgba(255, 255, 255, 0.5);
       margin: 4px 0 0.8em 0;
     }
-    &:after{
+    &:after {
     }
   }
-//   .line {
-//    width:100px;
-// //    max-height: 10vh;
-//     height: 100px;
-//    border-left: 1px solid red;
-//    margin-left:50%;
-   
-// }
+  //   .line {
+  //    width:100px;
+  // //    max-height: 10vh;
+  //     height: 100px;
+  //    border-left: 1px solid red;
+  //    margin-left:50%;
 
-.button {
-		@include vendor('transition', ('background-color 0.35s ease-in-out', 'color 0.35s ease-in-out'));
-		-webkit-appearance: none;
-		position: relative;
-		display: inline-block;
-		border: 0;
-		background: #35b88f;
-		color: #fff;
-		text-shadow: 0 0 0.5px rgba(255, 255, 255, 0.25);
-		cursor: pointer;
-		text-decoration: none;
-		outline: 0;
-		padding: 1em 2em 1em 2em;
-		text-align: center;
-		border-radius: 3em;
-        font-weight: 400;
-        margin-top:10px
-		&:hover {
-			background: #45c89f;
-		}
+  // }
 
-		&.style2 {
-			color: #3a3939;
-			background: #fff;
-			box-shadow: inset 0 0 0 1px #fff;
+  .button {
+    
+    -webkit-appearance: none;
+    position: relative;
+    display: inline-block;
+    border: 0;
+    background: #35b88f;
+    color: #fff;
+    text-shadow: 0 0 0.5px rgba(255, 255, 255, 0.25);
+    cursor: pointer;
+    text-decoration: none;
+    outline: 0;
+    padding: 1em 2em 1em 2em;
+    text-align: center;
+    border-radius: 3em;
+    font-weight: 400;
+    margin-top:10px &:hover {
+      background: #45c89f;
+    }
 
-			&:hover {
-				background: rgba(255, 255, 255, 0.1);
-				color: #fff !important;
-				text-shadow: 0 0 0.5px rgba(255, 255, 255, 0.25) !important;
-			}
-		}
+    &.style2 {
+      color: #3a3939;
+      background: #fff;
+      box-shadow: inset 0 0 0 1px #fff;
 
-		&.style3 {
-			background: none;
-			color: #3a3939;
-			box-shadow: inset 0 0 0 1px #dad9d9;
+      &:hover {
+        background: rgba(255, 255, 255, 0.1);
+        color: #fff !important;
+        text-shadow: 0 0 0.5px rgba(255, 255, 255, 0.25) !important;
+      }
+    }
 
-			&:hover {
-				background: rgba(58, 57, 57, 0.025);
-			}
-		}
-	}
+    &.style3 {
+      background: none;
+      color: #3a3939;
+      box-shadow: inset 0 0 0 1px #dad9d9;
+
+      &:hover {
+        background: rgba(58, 57, 57, 0.025);
+      }
+    }
+  }
 `;
 
 export const Banner = styled.section`
@@ -174,7 +172,7 @@ export const Feature = styled.section`
     align-content: center; 
     height: 100vh;
 
-    background-color: #9e554f;
+    background-color: #726D69;
 `;
 
 export const Portfolio = styled.section`
@@ -182,9 +180,80 @@ export const Portfolio = styled.section`
     flex-direction: column 
     justify-content: center;
     align-content: center; 
-    height: 100vh;
 
-    background-color: #726D69;
+    ///Take care of this at media query either remove or set to 100vh
+    height: 100%;
+    background-color: #9e554f;
+.card{
+  width: 100%;
+	overflow: hidden;
+  border-radius:0px;
+  margin:5px;
+}
+  
+       .card-img-top  {
+        width: 100%;
+        transform: scale(1);
+        transition: .3s ease-in-out;
+        border-top-left-radius: 0px;
+    border-top-right-radius: 0px;
+      }
+      .card-img-top:hover {
+        transform: scale(1.3);
+      }
+
+      .card-body{
+        background: #eeee
+      }
+      .btn{
+        border-radius: 0px;
+        color: #262262;
+       background-color: #eeee; 
+      border-color: #262262;
+      }
+    .btn .btn-secondary{
+      color: #262262;
+     
+      background-color: #eeee; 
+      border-color: #262262;
+    }
+    
+   .assure{
+    background-color: #eeee; 
+    margin:10px auto 10px auto;
+   }
+.join-sec{
+  background-color:#8775D1;
+  color:white;
+  padding: 100px 10px;
+  margin: 0 auto;
+  .join-btn {
+    
+    -webkit-appearance: none;
+    position: relative;
+    display: inline-block;
+    border: 0;
+    background: #35b88f;
+    color: #fff;
+    text-shadow: 0 0 0.5px rgba(255, 255, 255, 0.25);
+    cursor: pointer;
+    text-decoration: none;
+    outline: 0;
+    padding: 1em 2em 1em 2em;
+    text-align: center;
+    border-radius: 3em;
+    font-weight: 400;
+    margin-top:10px &:hover {
+      background: #45c89f;
+    }
+
+    &:hover {
+      background: #fff;
+      color: #4cbb76;
+    }
+  }
+}
+
 `;
 
 export const CTA = styled.section`
