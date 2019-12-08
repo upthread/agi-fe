00a28cc136f8e30507654051805038255ab8f124
Kyle Corbin <kylecorbin26@gmail.com>
2019-12-08 14:40:02 -0500

    Got started on the myDeals page, taking a break

--- src/Dashboard/Pages/MyDealsPage.js
+++ src/Dashboard/Pages/MyDealsPage.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const MyDeals = ()=>{
 return(
-    <div className="Dash-item">
+    <div className="Dash-item Deals-selectors">
         <h1>My Deals</h1>
+        <div className="selectors">
+            <select>
+                <option>Sort By Transaction</option>
+            </select>
+            <select>
+                <option>Sort By Status</option>
+            </select>
+            <button type="submit">Submit</button>
+        </div>
+       
     </div>
 );
 }
